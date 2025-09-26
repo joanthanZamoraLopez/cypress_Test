76a69877ee993ce4bbd207df78cb1ccb52f59809
Jonathan <jjzl9602_1102@hotmail.com>
2025-09-26 14:38:56 -0600

    format fixed

--- cypress/e2e/saucedemo_cart_stable.cy.js
+++ cypress/e2e/saucedemo_cart_stable.cy.js
@@ -1,13 +1,13 @@
-describe('Flujo de SauceDemo con tests separados', () => {
+describe("Flujo de SauceDemo con tests separados", () => {
 
   // ---------- LOGIN ANTES DE CADA TEST ----------
   beforeEach(() => {
-    cy.visit('https://www.saucedemo.com/')
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
-    cy.url().should('include', '/inventory.html')
-    cy.get('.inventory_list').should('be.visible')
+    cy.visit("https://www.saucedemo.com/")
+    cy.get("#user-name").type("standard_user")
+    cy.get("#password").type("secret_sauce")
+    cy.get("#login-button").click()
+    cy.url().should("include", "/inventory.html")
+    cy.get(".inventory_list").should("be.visible")
 
   })
 
